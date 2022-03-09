2049c470e6230469d2926bc35e81808f8544e0c8
ku1248 <101209771+ku1248@users.noreply.github.com>
2022-03-09 11:25:59 +0800

    Add files via upload

---
+++ GaussianFilter.h
@@ -0,0 +1,30 @@
+#ifndef GAUSSIAN_FILTER_H_
+#define GAUSSIAN_FILTER_H_
+#include <systemc>
+using namespace sc_core;
+
+#include "filter_def.h"
+
+class GaussianFilter : public sc_module {
+public:
+  sc_in_clk i_clk;
+  sc_in<bool> i_rst;
+  sc_fifo_in<unsigned char> i_r;
+  sc_fifo_in<unsigned char> i_g;
+  sc_fifo_in<unsigned char> i_b;
+  sc_fifo_out<int> o_result_r;
+  sc_fifo_out<int> o_result_g;
+  sc_fifo_out<int> o_result_b;
+
+
+  SC_HAS_PROCESS(GaussianFilter);
+  GaussianFilter(sc_module_name n);
+  ~GaussianFilter() = default;
+
+private:
+  void do_filter();
+  int val_r;
+  int val_g;
+  int val_b;
+};
+#endif
